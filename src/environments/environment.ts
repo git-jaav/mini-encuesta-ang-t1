f396bca5d9617b24f48a4a71183a63b7d07bbb8d
git-jaav <araucovillar@gmail.com>
2021-05-18 23:20:48 -0500

    add docker file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,8 +9,10 @@ export const environment = {
 
 export const api = {
 
-  //domain_api: "http://localhost:8088/api/business/miniencuesta/v1/"
-  domain_api: "http://loadbalancer-me-v1-125389634.us-east-1.elb.amazonaws.com/api/business/miniencuesta/v1/"
+  domain_api: "http://localhost:8088/api/business/miniencuesta/v1/"
+  //domain_api: "http://loadbalancer-me-v1-125389634.us-east-1.elb.amazonaws.com/api/business/miniencuesta/v1/"
+  //domain_api: "https://wfjy05poxe.execute-api.us-east-1.amazonaws.com/dev/api/business/miniencuesta/v1/"
+  
 }
 
 
